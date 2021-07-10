18fbc3a70b50bd54936755b5faa035c7ffc6eba5
imenesg <imenesgabriel@gmail.com>
2021-07-10 19:56:34 -0300

    organizing folders

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+var showmwnu = document.querySelector(".showMenu")
+var hidemwnu = document.querySelector(".hideMenu")
+var ul4 = document.querySelector(".ul")
+
+
+
+function show(){
+    
+    ul4.classList.add("jsMenushow")
+    console.log("ava")
+}
+function hide(){
+    
+    ul4.classList.remove("jsMenushow")
+    
+}
